6a7245e9901962d5b227ff41ca48973697ee1074
LEAP\Gabriel.McHugh <gabriel.t.mchugh@gmail.com>
2023-01-24 17:48:53 +1000

    measuring tools list is now exapndable

--- src/app/measuring-tools-list/measuring-tools-list.component.ts
+++ src/app/measuring-tools-list/measuring-tools-list.component.ts
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeasuringToolsListComponent implements OnInit {
 
+  array: number[];
+
   constructor() {
+    this.array = [1]
   }
 
   ngOnInit(): void {
+  }
+
+  addServingInput() {
+    this.array.push(this.array.length + 1)
   }
 
 }
